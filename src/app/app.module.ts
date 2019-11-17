20d11df972eef0ee447325989fae18ab86deda7c
Сергей Лабузов <sergeylabuzav@gmail.com>
2019-11-17 17:22:32 +0300

    Test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { ProductListComponent } from './private/product/product-list/product-list.component';
+import { HomeComponent } from './private/home/home.component';
+import { EmployeeListComponent } from './private/employee/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    HomeComponent,
+    EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      [
+        {
+          path: '',
+          component: HomeComponent
+        },
+        {
+          path: 'product',
+          component: ProductListComponent
+        },
+        {
+          path: 'employee',
+          component: EmployeeListComponent
+        }
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
